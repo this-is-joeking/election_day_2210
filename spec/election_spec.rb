6346b15d776baeaf26b274e5421bee20417d6d0b
Joe King <j.king92@gmail.com>
2022-11-01 08:38:16 -0700

    test: that the year can be returned

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -9,4 +9,10 @@ describe Election do
 
     expect(election).to be_a Election
   end
+
+  it 'can return the year of the election' do
+    election = Election.new('2022')
+
+    expect(election.year).to eq('2022')
+  end
 end
