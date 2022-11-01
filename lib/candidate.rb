89eee06e25e131737c253aaa8d0bf46b2a088763
Joe King <j.king92@gmail.com>
2022-11-01 08:11:58 -0700

    feat: Add party attribute and attr reader

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,6 +1,9 @@
 class Candidate
-  attr_reader :name
+  attr_reader :name,
+              :party
+              
   def initialize(candidate_details)
     @name = candidate_details[:name]
+    @party = candidate_details[:party]
   end
 end
