7ab831fb00d7b5d9bf79916c478173c7ab77d1eb
Joe King <j.king92@gmail.com>
2022-11-01 09:06:03 -0700

    feat: Add open? method to tell if race is still open

--- lib/race.rb
+++ lib/race.rb
@@ -5,11 +5,16 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(candidate_details)
     candidate = Candidate.new(candidate_details)
     @candidates.push(candidate)
     candidate
+  end
+
+  def open?
+    @open
   end
 end
