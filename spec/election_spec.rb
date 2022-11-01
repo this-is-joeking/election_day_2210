272a6b7bed89ab0d9790ffbba329baed0ea30b1f
Joe King <j.king92@gmail.com>
2022-11-01 09:03:41 -0700

    refactor: Improve tests for add race method

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -27,12 +27,9 @@ describe Election do
       election = Election.new('2022')
       race1 = Race.new("Virginia District 4 Representative")
       race2 = Race.new("Texas Governor")
-      election.add_race(race1)
 
-      expect(election.races).to eq([race1])
-
-      election.add_race(race2)
-
+      expect(election.add_race(race1)).to eq([race1])
+      expect(election.add_race(race2)).to eq([race1, race2])
       expect(election.races).to eq([race1, race2])
     end
   end
