e4bd9862eededdc2f3943f9c41373f317a8e9204
Joe King <j.king92@gmail.com>
2022-11-01 08:09:11 -0700

    test: that the candidates name can be returned

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -7,4 +7,10 @@ describe Candidate do
 
     expect(candidate).to be_a(Candidate)
   end
+
+  it 'returns the name' do
+    candidate = Candidate.new({name: "Diana D", party: :democrat})
+
+    expect(candidate.name).to eq("Diana D")
+  end
 end
