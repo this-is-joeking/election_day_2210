e20f41f75f47cf60e9735a66e8e93a36586bd412
Joe King <j.king92@gmail.com>
2022-11-01 08:53:47 -0700

    test: that the vote counts method returns a hash with candidates name and number of votes

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -55,4 +55,28 @@ describe Election do
       expect(election.candidates).to eq([candidate1, candidate2, candidate3, candidate4, candidate5])
     end
   end
+
+  describe '#vote_counts' do
+    it 'returns hash of candidate names and their vote count' do
+      election = Election.new('2022')
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+      election.add_race(race1)
+      election.add_race(race2)
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+      4.times {candidate1.vote_for!}
+      1.times {candidate2.vote_for!}
+      10.times {candidate3.vote_for!}
+      6.times {candidate4.vote_for!}
+      6.times {candidate5.vote_for!}
+
+      expect(election.vote_counts).to eq({
+        "Diana D"=>4, "Roberto R"=>1, "Diego D"=>10, "Rita R"=>6, "Ida I"=>6
+        })
+    end
+  end
 end
