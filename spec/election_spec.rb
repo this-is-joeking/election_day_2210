e6834b9c46b7cbbb06bf9a7337a9b4a9e68944b8
Joe King <j.king92@gmail.com>
2022-11-01 08:42:22 -0700

    test: that races can be added to the election

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -21,4 +21,19 @@ describe Election do
 
     expect(election.races).to eq([])
   end
+
+  describe '#add_race' do
+    it 'can add races' do
+      election = Election.new('2022')
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+      election.add_race(race1)
+
+      expect(election.races).to eq([race1])
+
+      election.add_race(race2)
+
+      expect(election.races).to eq([race1, race2])
+    end
+  end
 end
