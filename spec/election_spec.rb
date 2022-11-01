9b6d469c0a90837478b4f3506ba332c125327b7d
Joe King <j.king92@gmail.com>
2022-11-01 08:47:22 -0700

    test: that all candidates in an election can be viewed

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -36,4 +36,23 @@ describe Election do
       expect(election.races).to eq([race1, race2])
     end
   end
+
+  describe '#candidates' do
+    it 'returns array of all candidates in the election' do
+      election = Election.new('2022')
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+
+      expect(election.candidates).to eq([])
+      election.add_race(race1)
+      election.add_race(race2)
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+
+      expect(election.candidates).to eq([candidate1, candidate2, candidate3, candidate4, candidate5])
+    end
+  end
 end
