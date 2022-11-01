0694cbd76b6cc1976f6de0801910943df555e393
Joe King <j.king92@gmail.com>
2022-11-01 08:06:09 -0700

    test: that candidate can be instantiated with arguments

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -3,7 +3,7 @@ require './lib/candidate'
 
 describe Candidate do
   it 'exists' do
-    candidate = Candidate.new
+    candidate = Candidate.new({name: "Diana D", party: :democrat})
 
     expect(candidate).to be_a(Candidate)
   end
